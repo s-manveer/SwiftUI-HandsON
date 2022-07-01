c5ae4cabfd1c773ec12a178a09a3450282b6706d
Manveer Singh <manveer.singh11@gmail.com>
2022-07-02 02:06:01 +0530

    Initial commit

---
+++ SwiftUI-practice/SwiftUI-practice/SwiftUI_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_practiceApp.swift
+//  SwiftUI-practice
+//
+//  Created by Manveer Singh on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
